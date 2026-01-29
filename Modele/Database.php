0494839f137ff2272182ea705c00e2ddb6a447d7
mbock_martine <martinedalore2008@yahoo.fr>
2026-01-29 15:47:13 +0100

    création de la page database pour me connecter avec ma base de données

--- Modele/Database.php
+++ Modele/Database.php
@@ -1,27 +1,27 @@
 <?php
 
 
-function getConnexion(): PDO
-{
+    function getConnexion(): PDO
+    {
 
-    $tParam = parse_ini_file("param/param.ini", true);
+        $tParam = parse_ini_file("param/param.ini", true);
 
 
-    extract($tParam);
+        extract($tParam);
 
-    $dsn = "mysql:dbname=$DBNAME;host=$DBHOST;port=$DBPORT";
+        $dsn = "mysql:dbname=$DBNAME;host=$DBHOST;port=$DBPORT";
 
-    try {
-        $option = array(
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-        );
+        try {
+            $option = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            );
 
-        $connexion = new PDO($dsn, $DBUSER, $DBPASS, $option);
+            $connexion = new PDO($dsn, $DBUSER, $DBPASS, $option);
 
-        return $connexion;
-    } catch (PDOException $e) {
-        die("Echec de Connexion" . $e->getMessage());
+            return $connexion;
+        } catch (PDOException $e) {
+            die("Echec de Connexion" . $e->getMessage());
+        }
     }
-}
 ?>
